e23f7968f2cc7d1db789a796d1470a4556e761b7
Darell Mona <s2200064@student.unklab.ac.id>
2024-04-21 03:34:57 +0800

    fix transaction time problem

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  transactionOptions: {
+    maxWait: 25000,
+    timeout: 30000,
+    isolationLevel: Prisma.TransactionIsolationLevel.Serializable,
+  },
+});
 
 export default prisma;
